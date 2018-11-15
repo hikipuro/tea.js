3362053bf449d9d6aae51092ca0a94377ba3ff98
hikipuro <hikipuro@hotmail.com>
2018-11-15 18:46:33 +0900

    fix: add component menu position

--- src/tea/editor/commands/ObjectInspectorCommand.ts
+++ src/tea/editor/commands/ObjectInspectorCommand.ts
@@ -91,10 +91,11 @@ export class ObjectInspectorCommand {
 		var button = this.inspectorView.$el.querySelector("div.AddComponent button");
 		if (button) {
 			var rect = button.getBoundingClientRect();
-			console.log(rect);
+			//console.log(rect);
 			//contextMenu.show(rect.left, rect.top);
-			var x = Math.floor(rect.left);
-			var y = Math.floor(rect.bottom + 10);
+			var zoom = window.outerWidth / window.innerWidth;
+			var x = Math.floor(rect.left * zoom);
+			var y = Math.floor(rect.bottom * zoom + 10);
 			contextMenu.show(x, y);
 		} else {
 			contextMenu.show();
